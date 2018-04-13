a05e191105f26ca04eff5b5f78785c3093d16f62
Dan Cap <hello@danielcapobianco.com>
2018-04-13 16:24:25 -0400

    Remove gradle mistake, creating new project for that tutorial

--- src/main/java/gradletutorial/Greeter.java
+++
@@ -1,7 +0,0 @@
-package main.java.gradletutorial;
-
-public class Greeter {
-	public String sayHello() {
-		return "Hello world!";
-	}
-}
--- src/main/java/gradletutorial/HelloWorld.java
+++
@@ -1,5 +0,0 @@
-package main.java.gradletutorial;
-
-public class HelloWorld {
-
-}
