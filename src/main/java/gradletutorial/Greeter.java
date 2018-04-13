3635060d7133c4d538554ddf6b57f19e62d4c807
Dan Cap <hello@danielcapobianco.com>
2018-04-13 15:43:50 -0400

    Gradle short tutorial found at https://spring.io/guides/gs/gradle/#scratch

---
+++ src/main/java/gradletutorial/Greeter.java
@@ -0,0 +1,7 @@
+package main.java.gradletutorial;
+
+public class Greeter {
+	public String sayHello() {
+		return "Hello world!";
+	}
+}
---
+++ src/main/java/gradletutorial/HelloWorld.java
@@ -0,0 +1,5 @@
+package main.java.gradletutorial;
+
+public class HelloWorld {
+
+}
